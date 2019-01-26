e85c9224300e5cc6c6c328167f0b0f5a7e4e55e5
Matthew Lin <mlin367@yahoo.com>
2019-01-25 17:31:52 -0800

    Added hover effects for navbar sections

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -13,19 +13,19 @@ const NavBar = (props : NavBarProps) => (
     </div>
       <div className={styles.sections}>
         <div className="home">
-          <Link activeClass="active" className="section1" to="section1" spy={true} smooth={true} duration={500} >Home</Link>
+          <Link activeClass="active" className={styles.section1} to="section1" spy={true} offset={-80} smooth={true} duration={500} >Home</Link>
         </div>
         <div className="about">
-          <Link offset={50} activeClass="active" className="section2" to="section2" spy={true} smooth={true} duration={500} >About</Link>
+          <Link activeClass="active" className={styles.section2} to="section2" spy={true} offset={-60} smooth={true} duration={500} >About</Link>
         </div>
         <div className="projects">
-          <Link activeClass="active" className="section3" to="section3" spy={true} smooth={true} duration={500} >Projects</Link>
+          <Link activeClass="active" className={styles.section3} to="section3" spy={true} offset={-60} smooth={true} duration={500} >Projects</Link>
         </div>
-        <div className="resume">
+        <div className={styles.resume}>
           Resume
         </div>
-        <div className="contact">
-          <Link activeClass="active" className="section4" to="section4" spy={true} smooth={true} duration={500} >Contact</Link>
+        <div className={styles.section4}>
+          <Link activeClass="active" className="section4" to="section4" spy={true} offset={-60} smooth={true} duration={500} >Contact</Link>
         </div>
       </div>
   </nav>
