1eecc565a45465ac77751d1fef45a644752eb0ab
Matthew Lin <mlin367@yahoo.com>
2019-01-31 17:22:16 -0800

    added links and border on picture

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -5,21 +5,22 @@ const About = (props: {}) => (
   <div className={styles.about}>
     <h2 className={styles.like}>About</h2>
     <img
-      src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.15752-9/46879896_328645761199267_4156118004117012480_n.jpg?_nc_cat=104&_nc_ht=scontent-lax3-1.xx&oh=80eb43c742b67ec0b231b54234dd06d0&oe=5CBC11E3"
+      src="https://s3.us-east-2.amazonaws.com/matthew-site/me.jpg"
       className={styles.selfie}
     />
     <p className={styles.description}>
-      Hi, my name is Matthew Lin. I graduated from UCSD with a chemical
+      Hi, my name is Matthew Lin and I graduated from UCSD with a chemical
       engineering degree. Although I enjoyed the skills that the field provided
       me, I yearned for something more practical and decided to pursue
-      programming. Since then, I've build full stack applications, utilizing
+      programming. Since then, I've built full stack applications, utilizing
       both front-end and back-end technologies, and I am constantly exposing
-      myself to new tech stacks as I enjoy learning and new challenges. I am
-      aiming to become a full-fledged software engineer, with an emphasis on
-      front-end and full stack development. Please take a look at resume to get
-      a more in-depth look on my skills and projects. If any of it interests
-      you, feel free to get in touch with me with any of the contact information
-      I have provided. I hope you have a good day!
+      myself to new tech stacks as I enjoy learning and new challenges.
+      <br />
+      <br />I am aiming to become a full-fledged software engineer, with an
+      emphasis on front-end and full stack development. Please take a look at
+      resume to get a more in-depth look on my skills and projects. If any of it
+      interests you, feel free to get in touch with me through any of the
+      contact information I have provided. I hope you have a good day!
     </p>
   </div>
 );
--- client/src/components/Projects.tsx
+++ client/src/components/Projects.tsx
@@ -5,24 +5,42 @@ const Projects = (props: {}) => (
   <div className={styles.projects}>
     <h2 className={styles.projectsTitle}>Projects</h2>
     <div className={styles.shoedidas}>
-      <img src="https://s3-us-west-1.amazonaws.com/shoedidas-static/assets/img/shoedidas_white_5.svg" className={styles.shoedidasLogo}></img>
+      <img
+        src="https://s3-us-west-1.amazonaws.com/shoedidas-static/assets/img/shoedidas_white_5.svg"
+        className={styles.shoedidasLogo}
+      />
       <h3 className={styles.shoedidasTitle}>shoedidas</h3>
-      <p className={styles.shoedidasDescription}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, incidunt alias consequatur ex ducimus adipisci inventore voluptatibus facere commodi nemo doloremque sunt magnam at provident debitis maiores perferendis ipsam numquam.</p>
+      <p className={styles.shoedidasDescription}>
+        shoedidas is a full stack application built by 4 team members. The aim
+        of the site is to replicate the adidas product page to look as close to
+        the real thing as possible. My component was the ratings/reviews section
+        and the footer. Feel free to take a look at my github and the link to
+        find out more about this application!
+      </p>
       <div className={styles.shoedidasLinks}>
-        <a>Github</a>
-        <a>Link</a>
+        <a className={styles.shoeGit} target="_blank" href="https://github.com/mlin367/Adidas-Clone">Github</a>
+        <a className={styles.shoeLink} target="_blank" href="http://54.146.229.91/">Link</a>
       </div>
     </div>
     <div className={styles.gomoku}>
-      <img src="https://s3.us-east-2.amazonaws.com/matthew-site/gomokuPic.JPG" className={styles.gomokuLogo}></img>
+      <img
+        src="https://s3.us-east-2.amazonaws.com/matthew-site/gomokuPic.JPG"
+        className={styles.gomokuLogo}
+      />
       <h3 className={styles.gomokuTitle}>Gomoku</h3>
-      <p className={styles.gomokuDescription}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi blanditiis ex itaque officiis molestiae, necessitatibus perferendis quas amet facere rerum repudiandae ratione at sint consectetur? Iste quidem nostrum porro doloribus?</p>
+      <p className={styles.gomokuDescription}>
+        Gomoku is a game played on a 15 x 15 board played by two players where
+        the first player to connect five stones in a row wins. This application
+        was built utilizing web sockets and so two players can play against each
+        other in real time. Feel free to take a look at my github and the link
+        to find out more about this application!
+      </p>
       <div className={styles.gomokuLinks}>
-        <a>Github</a>
-        <a>Link</a>
+        <a className={styles.goGit} target="_blank" href="https://github.com/mlin367/MVP-2.0">Github</a>
+        <a className={styles.goLink} target="_blank" href="">Link</a>
       </div>
     </div>
   </div>
-)
+);
 
-export default Projects
+export default Projects;
